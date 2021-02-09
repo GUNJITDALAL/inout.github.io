c5a33c8e758be3b8d91edd4cdcfb63d84caafac3
gunjit <73329409+GUNJITDALAL@users.noreply.github.com>
2021-02-09 22:16:20 +0530

    Add files via upload

---
+++ inoutput.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    char data;
+    int num;
+    printf("Enter any input");
+    scanf("%d",&num);
+    printf("Enter your number=%d/n");
+    data=getch();
+    printf("Enter single character/n");
+    printf("Your character is =/n");
+    putch(data);
+
+
+
+    return 0;
+}
